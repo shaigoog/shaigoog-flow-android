5f31697f3c6f763bb2ce5f45374708195fe8c2f2
shaigoog <sedeegshaigoog@gmail.com>
2026-06-22 23:46:18 +0400

    Add Compose colors

---
+++ app/src/main/kotlin/com/shaigoog/flow/ui/theme/Color.kt
@@ -0,0 +1,10 @@
+package com.shaigoog.flow.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+internal val Navy = Color(0xFF10263D)
+internal val Teal = Color(0xFF1C8B82)
+internal val Gold = Color(0xFFD5A63F)
+internal val Sand = Color(0xFFF2E7D5)
+internal val Mist = Color(0xFFF4F6F8)
+internal val Ink = Color(0xFF18212B)
